34dbb003c7f7364f19568571b18d2382a9a896e7
Martin Markov <martin.markov@weareaccess.co.uk>
2022-06-28 17:56:50 +0100

    Example of using the symfony/filesystem package to create a file.

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
+require 'vendor/autoload.php';
+
+use Symfony\Component\Filesystem\Filesystem;
 
 echo 'Test';
+
+$file_system = new Filesystem();
+
+$file_system->dumpFile(__DIR__ . '/new_file.txt', 'Test content.');
